db9f577b3d103cad1422535a76f26600745cb9fe
NSTI <NSTI@gmail.com>
2024-08-13 16:44:23 +0530

    change the middle page

---
+++ src/app/pages/forts/forts.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FortsComponent } from './forts.component';
+
+describe('FortsComponent', () => {
+  let component: FortsComponent;
+  let fixture: ComponentFixture<FortsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [FortsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(FortsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
